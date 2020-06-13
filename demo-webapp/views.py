f4bd9741c590095333d27fce6e64f7c0800ad485
Sebastien <sebastien@pittet.org>
2020-06-13 18:45:26 +0200

    Correct problem with IP address

--- demo-webapp/views.py
+++ demo-webapp/views.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import socket
 import time
+from requests import get
 
 demowebapp = Flask(__name__)
 
@@ -8,7 +9,7 @@ demowebapp = Flask(__name__)
 @demowebapp.route("/index")
 def main():
     hostname = socket.gethostname()
-    ip_addr = socket.gethostbyname(hostname)
+    ip_addr = get('https://api.ipify.org').text
     time_request = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
     server = {'hostname': hostname,
               'ipaddr': ip_addr,
@@ -17,6 +18,7 @@ def main():
     return render_template('index.html',
                            title="Exoscale powered by Lenovo",
                            server = server)
+
 
 if __name__ == "__main__":
     demowebapp.run()
