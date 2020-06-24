072302ad74f406193d8b0bcc8e354ebcbe01b781
Sebastien <sebastien@pittet.org>
2020-06-24 16:05:42 +0200

    Avoid caching on this view

--- demo-webapp/views.py
+++ demo-webapp/views.py
@@ -2,11 +2,29 @@ from flask import Flask, render_template
 import socket
 import time
 from requests import get
+from flask import make_response
+from functools import wraps, update_wrapper
+from datetime import datetime
 
 demowebapp = Flask(__name__)
 
+
+def nocache(view): # avoid caching
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+    return update_wrapper(no_cache, view)
+
+
+
 @demowebapp.route("/")
 @demowebapp.route("/index")
+@nocache # avoid caching on this view
 def main():
     hostname = socket.gethostname()
     ip_addr = get('https://api.ipify.org').text
@@ -16,7 +34,7 @@ def main():
               'time_request':time_request}
 
     return render_template('index.html',
-                           title="Exoscale ping times",
+                           title="Exoscale 'ping times'",
                            server = server_name)
 
 
