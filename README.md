008ae674bacb169ad4270add0b7fec81e7338a78
Sébastien Pittet <sebastien@pittet.org>
2021-08-01 17:21:08 +0200

    package, travis, test to write

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # demo-webapp
 
+[![Total alerts](https://img.shields.io/lgtm/alerts/g/SebastienPittet/demo-webapp.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/SebastienPittet/demo-webapp/alerts/)
+
+[![Language grade: Python](https://img.shields.io/lgtm/grade/python/g/SebastienPittet/demo-webapp.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/SebastienPittet/demo-webapp/context:python)
+
+
 This application is written in Python and based on [Flask](https://flask.palletsprojects.com/).
 The template uses [Bootstrap framework](https://getbootstrap.com/).
 It only show some content for **demo purpose**.
 
 In its initial release, it is mainly a wrapper of a Single-page-Application (SPA) stored on Exoscale [Simple Object Storage (SOS)](https://www.exoscale.com/object-storage/) but can evolve differently in the future.
+
+From the release of August 1st, it fetch a random advice from [adviceslip.com Open API](https://api.adviceslip.com/).
---
+++ setup.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+"""
+A simple web application used in demo.
+"""
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='demo-webapp',
+   version='0.0.2',
+   author='Sebastien Pittet',
+   author_email='sebastien@pittet.org',
+   description='a simple web-app based on flask',
+   long_description=long_description,
+   url='https://github.com/SebastienPittet/demo-webapp',
+   keywords='demo exoscale',
+   packages=find_packages(),
+   license='LICENSE.txt',
+   platforms='any',
+   install_requires=[
+       "Flask",
+       "requests",
+   ],
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience'
+    ]
+)
