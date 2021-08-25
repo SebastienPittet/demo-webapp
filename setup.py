7d8b53e56d67ee16b74801aa954eb60a9231b8bd
Sébastien Pittet <sebastien@pittet.org>
2021-08-25 23:51:30 +0200

    Add gunicorn, remove old Dockerfile

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst', 'r') as f:
 
 setup(
    name='demo-webapp',
-   version='0.0.3',
+   version='0.0.4',
    author='Sebastien Pittet',
    author_email='sebastien@pittet.org',
    description='a simple web-app based on flask',
@@ -22,14 +22,13 @@ setup(
    install_requires=[
        "Flask",
        "requests",
+       "gunicorn"
    ],
    include_package_data=True,
    classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.4',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Development Status :: 3 - Alpha',
